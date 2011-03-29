84c171ac8ff0b1bb9ff43778fec9a0da019d3428
Richard Scott Smith <scott@thescene.com>
2011-03-29 14:19:16 -0700

    Attempt to illustrate using Tiles with Spring MVC.  This is supposed to use a Controller per Tile.  Not completely there yet, but it compiles and runs.  None of the view resolve right now.

---
+++ src/main/java/net/raescott/tilesexample/MainController.java
@@ -0,0 +1,28 @@
+package net.raescott.tilesexample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Richard Scott Smith <scott@thescene.com>
+ */
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "page1", method = RequestMethod.GET)
+	public String get1() {
+		return "main";
+	}
+
+	@RequestMapping(value = "page2", method = RequestMethod.GET)
+	public String get2() {
+		return "index";
+	}
+
+	@RequestMapping(value = "page3", method = RequestMethod.GET)
+	public String get3() {
+		return "standardLayout";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : main
+    Created on : Mar 29, 2011, 12:38:12 PM
+    Author     : Richard Scott Smith <scott@thescene.com>
+--%>
+
+<%@page contentType="text/html" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title>Main Page</title>
+    </head>
+    <body>
+		Main Page
+    </body>
+</html>
