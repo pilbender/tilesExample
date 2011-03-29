9cab472e0244cc44417935133caeb918a8a1bd27
Richard Scott Smith <scott@thescene.com>
2011-03-29 16:40:11 -0700

    Still working on this but we are closer.

--- src/main/java/net/raescott/tilesexample/MainController.java
+++ src/main/java/net/raescott/tilesexample/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
+	/*
 	@RequestMapping(value = "page1", method = RequestMethod.GET)
 	public String get1() {
 		return "main";
@@ -25,4 +26,11 @@ public class MainController {
 	public String get3() {
 		return "standardLayout";
 	}
+
+	@RequestMapping(value = "page4", method = RequestMethod.GET)
+	public String get4() {
+		return "/";
+	}
+	 *
+	 */
 }
