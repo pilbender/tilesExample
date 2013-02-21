8190e9b5a7e77f2d7adb7c11fcb27540decece8b
Richard Scott Smith <scott.smith@isostech.com>
2013-02-21 14:21:39 -0700

    Added a Model attribute to the footer.jsp page, which is in turn brought in by the footerController, which is brought into the main template page.
    This fully illustrates the controller per tile idea.

--- src/main/java/net/raescott/tilesexample/MainController.java
+++ src/main/java/net/raescott/tilesexample/MainController.java
@@ -1,7 +1,7 @@
 package net.raescott.tilesexample;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,14 +24,13 @@ public class MainController {
 	}
 
 	@RequestMapping(value = "mainController", method = RequestMethod.GET)
-	public String get3(ModelMap model) {
-		model.addAttribute("firstName", "Scott");
-		model.addAttribute("lastName", "Smith");
+	public String get3() {
 		return "main";
 	}
 
 	@RequestMapping(value = "footerController", method = RequestMethod.GET)
-	public String get4() {
+	public String get4(Model model) {
+		model.addAttribute("name", "Scott Smith");
 		return "footer";
 	}
 }
--- src/main/webapp/WEB-INF/views/footer.jsp
+++ src/main/webapp/WEB-INF/views/footer.jsp
@@ -1,1 +1,2 @@
 @Copyright 2008-2009
+Brought to you by ${name}
